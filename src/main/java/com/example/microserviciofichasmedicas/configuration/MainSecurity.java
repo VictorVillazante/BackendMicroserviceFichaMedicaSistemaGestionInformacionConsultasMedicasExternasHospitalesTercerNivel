ad0fae648d02bb5b0cfd416ce410600f810a6e8f
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-11-24 08:47:36 -0400

    [Fix] cors configuration

--- src/main/java/com/example/microserviciofichasmedicas/configuration/MainSecurity.java
+++ src/main/java/com/example/microserviciofichasmedicas/configuration/MainSecurity.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,9 +37,21 @@ public class MainSecurity {
         //         .anyRequest().authenticated();
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/fichas-medicas/**","/fichas-medicas","/fichas-medicas/paciente/*","/fichas-medicas/medico/*","/fichas-medicas/detalle/paciente/*")
+                .requestMatchers("/fichas-medicas/**","/fichas-medicas","/fichas-medicas/paciente/*","/fichas-medicas/medico/*","/fichas-medicas/detalle/paciente/*","/**")
                 .permitAll()
                 .anyRequest().authenticated();
         return http.build();
+    }
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Cambia esto por tu origen permitido
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true); // Habilita esto si necesitas enviar credenciales
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.addExposedHeader("Message");
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/example/microserviciofichasmedicas/service/ConverterVariablesService.java
+++ src/main/java/com/example/microserviciofichasmedicas/service/ConverterVariablesService.java
@@ -1,7 +1,9 @@
 package com.example.microserviciofichasmedicas.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ConverterVariablesService {
     @Value("${aws.s3.bucket.name}")
     private String bucketName;
