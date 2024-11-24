9d1b494775f97004494e93873e01227f2d87d19b
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-11-24 09:28:21 -0400

    [Fix] agregando mas configuracionees de cors

--- src/main/java/com/example/microserviciofichasmedicas/configuration/MainSecurity.java
+++ src/main/java/com/example/microserviciofichasmedicas/configuration/MainSecurity.java
@@ -21,37 +21,37 @@ public class MainSecurity {
    
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        // http.cors().configurationSource( request -> {
-        //     CorsConfiguration corsConfiguration = new CorsConfiguration();
-        //     corsConfiguration.setAllowedOrigins(List.of("*"));
-        //     corsConfiguration.setAllowedOriginPatterns(List.of("*"));
-        //     corsConfiguration.setAllowedMethods(List.of("HEAD","GET","POST","PUT","DELETE","PATCH","OPTIONS"));
-        //     corsConfiguration.setAllowCredentials(false);
-        //     corsConfiguration.addExposedHeader("Message");
-        //     corsConfiguration.setAllowedHeaders(List.of("*"));
-        //     return corsConfiguration;
-        // }).and().csrf().disable()
-        //         .authorizeRequests()
-        //         .requestMatchers("/fichas-medicas/*","/fichas-medicas","/fichas-medicas/paciente/*","/fichas-medicas/medico/*","/fichas-medicas/detalle/paciente/*")
-        //         .permitAll()
-        //         .anyRequest().authenticated();
-        http.cors().and().csrf().disable()
+        http.cors().configurationSource( request -> {
+            CorsConfiguration corsConfiguration = new CorsConfiguration();
+            corsConfiguration.setAllowedOrigins(List.of("*"));
+            corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+            corsConfiguration.setAllowedMethods(List.of("HEAD","GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+            corsConfiguration.setAllowCredentials(false);
+            corsConfiguration.addExposedHeader("Message");
+            corsConfiguration.setAllowedHeaders(List.of("*"));
+            return corsConfiguration;
+        }).and().csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/fichas-medicas/**","/fichas-medicas","/fichas-medicas/paciente/*","/fichas-medicas/medico/*","/fichas-medicas/detalle/paciente/*","/**")
+                .requestMatchers("/fichas-medicas/*","/fichas-medicas","/fichas-medicas/paciente/*","/fichas-medicas/medico/*","/fichas-medicas/detalle/paciente/*","/**")
                 .permitAll()
                 .anyRequest().authenticated();
+        // http.cors().and().csrf().disable()
+        //         .authorizeRequests()
+        //         .requestMatchers("/fichas-medicas/**","/fichas-medicas","/fichas-medicas/paciente/*","/fichas-medicas/medico/*","/fichas-medicas/detalle/paciente/*","/**")
+        //         .permitAll()
+        //         .anyRequest().authenticated();
         return http.build();
     }
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // Cambia esto por tu origen permitido
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowCredentials(true); // Habilita esto si necesitas enviar credenciales
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.addExposedHeader("Message");
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    // @Bean
+    // public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     CorsConfiguration configuration = new CorsConfiguration();
+    //     configuration.setAllowedOrigins(List.of("*")); // Cambia esto por tu origen permitido
+    //     configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    //     configuration.setAllowCredentials(true); // Habilita esto si necesitas enviar credenciales
+    //     configuration.setAllowedHeaders(List.of("*"));
+    //     configuration.addExposedHeader("Message");
+    //     source.registerCorsConfiguration("/**", configuration);
+    //     return source;
+    // }
 }
